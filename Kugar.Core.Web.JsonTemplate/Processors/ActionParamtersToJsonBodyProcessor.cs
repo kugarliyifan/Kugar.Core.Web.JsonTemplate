b39f2afeb149c3465127112465e89bfd30ec3765
启天 <@KUGAR>
2023-04-03 13:59:12 +0800

    增加了当出现重复的参数或者重复的action时,抛出的错误中,记录是哪个action报错的

--- Kugar.Core.Web.JsonTemplate/Processors/ActionParamtersToJsonBodyProcessor.cs
+++ Kugar.Core.Web.JsonTemplate/Processors/ActionParamtersToJsonBodyProcessor.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Reflection;
 using System.Xml;
 using Kugar.Core.ExtMethod;
+using Kugar.Core.Log;
 using Kugar.Core.Web.JsonTemplate.Helpers;
 using Microsoft.AspNetCore.Mvc;
 using NJsonSchema;
@@ -33,152 +34,172 @@ namespace Kugar.Core.Web.JsonTemplate.Processors
                 //context.OperationDescription.Operation.Tags.Add("formbodyjson");
                 //var lst = c.OperationDescription.Operation.Parameters.ToArrayEx();
 
-                if (!c.ApiDescription.ActionDescriptor.EndpointMetadata.Any(x => x is ConsumesAttribute))
+                try
                 {
-                    c.OperationDescription.Operation.Consumes.Add("application/json-patch+json");
-                    c.OperationDescription.Operation.Consumes.Add("application/json");
-                    c.OperationDescription.Operation.Consumes.Add("application/*+json");
-                }
-
-                var args = context.Parameters.Where(x => isFromBody(x.Key)).ToArrayEx();
-
-                context.OperationDescription.Operation.Parameters.Remove(x=>args.Any(y=>y.Key.Name==x.Name));
-
-                var methodName = context.MethodInfo.DeclaringType.FullName + "." + context.MethodInfo.Name;
-
-                var methodXmlNode = ExpressionHelpers.XmlDoc?.GetElementsByTagName("member").AsEnumerable<XmlElement>()
-                    .Where(x => x.GetAttribute("name").StartsWith($"M:{methodName}"))
-                    .FirstOrDefault();
-                
-                var paramXmlNodes = (methodXmlNode?.GetElementsByTagName("param").AsEnumerable<XmlElement>().ToArrayEx())??Array.Empty<XmlElement>();
-
-                var jsonSechma = new JsonSchema();
-                
-                foreach (var p1 in args)
-                {
-                    if (p1.Value==null)
+                    if (!c.ApiDescription.ActionDescriptor.EndpointMetadata.Any(x => x is ConsumesAttribute))
                     {
-                        continue;
+                        c.OperationDescription.Operation.Consumes.Add("application/json-patch+json");
+                        c.OperationDescription.Operation.Consumes.Add("application/json");
+                        c.OperationDescription.Operation.Consumes.Add("application/*+json");
                     }
 
-                    var pschema = p1.Value.Schema;
+                    var args = context.Parameters.Where(x => isFromBody(x.Key)).ToArrayEx();
 
-                    var isRequired = p1.Value.IsRequired;
+                    context.OperationDescription.Operation.Parameters.Remove(x => args.Any(y => y.Key.Name == x.Name));
 
-                    if (p1.Key.GetCustomAttribute<RequiredAttribute>()!=null)
+                    var methodName = context.MethodInfo.DeclaringType.FullName + "." + context.MethodInfo.Name;
+
+                    var methodXmlNode = ExpressionHelpers.XmlDoc?.GetElementsByTagName("member")
+                        .AsEnumerable<XmlElement>()
+                        .Where(x => x.GetAttribute("name").StartsWith($"M:{methodName}"))
+                        .FirstOrDefault();
+
+                    var paramXmlNodes =
+                        (methodXmlNode?.GetElementsByTagName("param").AsEnumerable<XmlElement>().ToArrayEx()) ??
+                        Array.Empty<XmlElement>();
+
+                    var jsonSechma = new JsonSchema();
+
+                    foreach (var p1 in args)
                     {
-                        isRequired = true;
-                    }
-
-                    var description = p1.Value.Description;
-
-                    if (paramXmlNodes.Any(x=>x.GetAttribute("name")==p1.Value.Name))
-                    {
-                        var tmp = paramXmlNodes.FirstOrDefault(x => x.GetAttribute("name") == p1.Value.Name)
-                            .InnerText;
-
-                        if (tmp.Length > (description?.Length??0))
+                        if (p1.Value == null)
                         {
-                            description = tmp;
+                            continue;
                         }
-                    }
-                    
-                    var prop = new JsonSchemaProperty()
-                    {
-                        Type = pschema?.Type ?? NSwagSchemeBuilder.NetTypeToJsonObjectType(p1.Key.ParameterType),
-                        IsNullableRaw =(pschema?.IsNullableRaw ?? p1.Value.IsNullableRaw)??false ,
-                        IsRequired = isRequired,
-                        Default = (pschema?.Default) ?? p1.Value.Default,
-                        Description = description,
-                        IsDeprecated = pschema?.IsDeprecated ?? p1.Value.IsDeprecated,
-                        Example = p1.Value.Example,// pschema.Example,
-                        Item = pschema?.Item ?? p1.Value.Item,
-                        MaxItems = pschema.MaxItems,
-                        MaxLength = pschema.MaxLength,
-                        Maximum = pschema.Maximum,
-                        MinLength = pschema.MinLength,
-                        Minimum = pschema.Minimum,
-                        MinItems = pschema.MinItems
-                    };
 
+                        var pschema = p1.Value.Schema;
 
+                        var isRequired = p1.Value.IsRequired;
 
-                    if ((pschema?.Items ?? p1.Value.Items).HasData())
-                    {
-                        var lst = (pschema?.Items ?? p1.Value.Items);
-
-                        foreach (var item in lst)
+                        if (p1.Key.GetCustomAttribute<RequiredAttribute>() != null)
                         {
-                            prop.Items.Add(item);
+                            isRequired = true;
                         }
-                    }
 
-                    if ((pschema?.Properties ?? p1.Value.Properties).HasData())
-                    {
-                        var lst = (pschema?.Properties ?? p1.Value.Properties);
+                        var description = p1.Value.Description;
 
-                        foreach (var item in lst)
+                        if (paramXmlNodes.Any(x => x.GetAttribute("name") == p1.Value.Name))
                         {
-                            prop.Properties.Add(item);
-                        }
-                    }
+                            var tmp = paramXmlNodes.FirstOrDefault(x => x.GetAttribute("name") == p1.Value.Name)
+                                .InnerText;
 
-                    if ((pschema?.Enumeration ?? p1.Value.Enumeration).HasData())
-                    {
-                        var lst = (pschema?.Enumeration ?? p1.Value.Enumeration);
-
-                        foreach (var item in lst)
-                        {
-                            prop.Enumeration.Add(item);
-                        }
-                    }
-
-                    if ((pschema?.EnumerationNames ?? p1.Value.EnumerationNames).HasData())
-                    {
-                        var lst = (pschema?.EnumerationNames ?? p1.Value.EnumerationNames);
-
-                        foreach (var item in lst)
-                        {
-                            prop.EnumerationNames.Add(item);
-                        }
-                    }
-
-                    jsonSechma.Properties.Add(p1.Value.Name, prop);
-                }
-
-                jsonSechma.Title = context.OperationDescription.Operation.OperationId.Replace("/", "_") + "_" + "RequestParamter";
-
-                context.Document.Components.Schemas.Add(jsonSechma.Title, jsonSechma);
-
-                context.OperationDescription.Operation.RequestBody = new OpenApiRequestBody()
-                {
-                    Description = "",
-                    IsRequired = true,
-                    Name = "body",
-                    Position = 1,
-                    Content =
-                    {
-                        ["application/json"]=new OpenApiMediaType()
-                        {
-                            Schema = new JsonSchema()
+                            if (tmp.Length > (description?.Length ?? 0))
                             {
-                                Reference = jsonSechma,
-
+                                description = tmp;
                             }
                         }
+
+                        var prop = new JsonSchemaProperty()
+                        {
+                            Type = pschema?.Type ?? NSwagSchemeBuilder.NetTypeToJsonObjectType(p1.Key.ParameterType),
+                            IsNullableRaw = (pschema?.IsNullableRaw ?? p1.Value.IsNullableRaw) ?? false,
+                            IsRequired = isRequired,
+                            Default = (pschema?.Default) ?? p1.Value.Default,
+                            Description = description,
+                            IsDeprecated = pschema?.IsDeprecated ?? p1.Value.IsDeprecated,
+                            Example = p1.Value.Example, // pschema.Example,
+                            Item = pschema?.Item ?? p1.Value.Item,
+                            MaxItems = pschema.MaxItems,
+                            MaxLength = pschema.MaxLength,
+                            Maximum = pschema.Maximum,
+                            MinLength = pschema.MinLength,
+                            Minimum = pschema.Minimum,
+                            MinItems = pschema.MinItems
+                        };
+
+
+
+                        if ((pschema?.Items ?? p1.Value.Items).HasData())
+                        {
+                            var lst = (pschema?.Items ?? p1.Value.Items);
+
+                            foreach (var item in lst)
+                            {
+                                prop.Items.Add(item);
+                            }
+                        }
+
+                        if ((pschema?.Properties ?? p1.Value.Properties).HasData())
+                        {
+                            var lst = (pschema?.Properties ?? p1.Value.Properties);
+
+                            foreach (var item in lst)
+                            {
+                                prop.Properties.Add(item);
+                            }
+                        }
+
+                        if ((pschema?.Enumeration ?? p1.Value.Enumeration).HasData())
+                        {
+                            var lst = (pschema?.Enumeration ?? p1.Value.Enumeration);
+
+                            foreach (var item in lst)
+                            {
+                                prop.Enumeration.Add(item);
+                            }
+                        }
+
+                        if ((pschema?.EnumerationNames ?? p1.Value.EnumerationNames).HasData())
+                        {
+                            var lst = (pschema?.EnumerationNames ?? p1.Value.EnumerationNames);
+
+                            foreach (var item in lst)
+                            {
+                                prop.EnumerationNames.Add(item);
+                            }
+                        }
+
+                        jsonSechma.Properties.Add(p1.Value.Name, prop);
                     }
-                };
 
-                var p = new OpenApiParameter();
-                p.Name = "body";
-                p.Kind = OpenApiParameterKind.Body;
-                p.Type = JsonObjectType.Object;
-                p.Schema = jsonSechma;
-                p.Reference = jsonSechma;
-                p.IsRequired = true;
-                //opt.Operation.Parameters.Add( doc.Document.Operations.Where(x=>x.Path.Contains("Add")).FirstOrDefault().Operation.Parameters[0].);
+                    jsonSechma.Title = context.OperationDescription.Operation.OperationId.Replace("/", "_") + "_" +
+                                       "RequestParamter";
 
-                context.OperationDescription.Operation.Parameters.Add(p);
+                    context.Document.Components.Schemas.Add(jsonSechma.Title, jsonSechma);
+
+                    context.OperationDescription.Operation.RequestBody = new OpenApiRequestBody()
+                    {
+                        Description = "",
+                        IsRequired = true,
+                        Name = "body",
+                        Position = 1,
+                        Content =
+                        {
+                            ["application/json"] = new OpenApiMediaType()
+                            {
+                                Schema = new JsonSchema()
+                                {
+                                    Reference = jsonSechma,
+
+                                }
+                            }
+                        }
+                    };
+
+                    var p = new OpenApiParameter();
+                    p.Name = "body";
+                    p.Kind = OpenApiParameterKind.Body;
+                    p.Type = JsonObjectType.Object;
+                    p.Schema = jsonSechma;
+                    p.Reference = jsonSechma;
+                    p.IsRequired = true;
+                    //opt.Operation.Parameters.Add( doc.Document.Operations.Where(x=>x.Path.Contains("Add")).FirstOrDefault().Operation.Parameters[0].);
+
+                    context.OperationDescription.Operation.Parameters.Add(p);
+                }
+                catch (ArgumentException e)
+                {
+                    LoggerManager.Default.Error($"生成{context.OperationDescription.Operation.OperationId}函数的文档出错");
+                    throw new Exception($"生成{context.OperationDescription.Operation.OperationId}函数的文档出错", e);
+                }
+                catch (Exception e)
+                {
+                    Debugger.Break();
+                    Console.WriteLine(context.OperationDescription.Operation.OperationId);
+                    throw ;
+                }
+
+                
             }
             return true;
         }
